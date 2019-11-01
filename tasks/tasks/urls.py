7ab28c40a9efe1eb1925348c872c38e8895abb35
bhaskargautam <bhaskar.iiita@gmail.com>
2019-11-01 14:00:14 +0530

    Bug fix: Missing import in urls.py

--- tasks/tasks/urls.py
+++ tasks/tasks/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
