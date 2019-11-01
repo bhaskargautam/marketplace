c4090d18c92a21d8acd04649ec8e682446ec416d
bhaskargautam <bhaskar.iiita@gmail.com>
2019-11-01 14:02:25 +0530

    Fixed import for settings.

--- tasks/tasks/urls.py
+++ tasks/tasks/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-import settings
+from tasks import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
